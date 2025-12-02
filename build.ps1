404f70cc7dce6b031c9682b9ead51e84fd4a6f05
Karsten A. M. Guenther <karsten.guenther@kamg.de>
2025-12-02 09:09:08 +0100

    feat: add scripts for native npm on windows

---
+++ build.ps1
@@ -0,0 +1,26 @@
+function Invoke-Bootstrap {
+    Invoke-RestMethod https://raw.githubusercontent.com/avengineers/bootstrap-installer/v1.17.2/install.ps1 | Invoke-Expression
+    . .\.bootstrap\bootstrap.ps1
+}
+
+## start of script
+# Always set the $InformationPreference variable to "Continue" globally,
+# this way it gets printed on execution and continues execution afterwards.
+$InformationPreference = "Continue"
+
+# Stop on first error
+$ErrorActionPreference = "Stop"
+
+Push-Location $PSScriptRoot
+Write-Output "Running in ${pwd}"
+
+try {
+    Invoke-Bootstrap
+
+    npm install
+}
+finally {
+   Pop-Location
+   Read-Host -Prompt "Press Enter to continue ..."
+}
+## end of script
